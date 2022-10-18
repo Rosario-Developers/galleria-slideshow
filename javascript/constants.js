191d41a00008cf7d0e45ae9bc17c858e19c414fa
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-10-18 10:40:36 -0300

    refactor: remove unnecessary code

--- javascript/constants.js
+++ javascript/constants.js
@@ -7,7 +7,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/700px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
     description:
       'The Starry Night is an oil-on-canvas painting by the Dutch Post-Impressionist painter Vincent van Gogh. Painted in June 1889, it depicts the view from the east-facing window of his asylum room at Saint-Rémy-de-Provence, just before sunrise, with the addition of an imaginary village.',
-    column: 0,
   },
   {
     name: 'The Storm on the Sea of Galilee',
@@ -17,7 +16,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/f/f3/Rembrandt_Christ_in_the_Storm_on_the_Lake_of_Galilee.jpg',
     description:
       'The Storm on the Sea of Galilee is a 1633 oil-on-canvas painting by the Dutch Golden Age painter Rembrandt van Rijn. It was previously in the Isabella Stewart Gardner Museum in Boston but was stolen in 1990 and remains missing.',
-    column: 0,
   },
   {
     name: 'Lady with an Ermine',
@@ -27,7 +25,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Lady_with_an_Ermine_-_Leonardo_da_Vinci_-_Google_Art_Project.jpg/480px-Lady_with_an_Ermine_-_Leonardo_da_Vinci_-_Google_Art_Project.jpg',
     description:
       'The Lady with an Ermine is a portrait painting widely attributed to the Italian Renaissance artist Leonardo da Vinci. Dated to c. 1489–1491, the work is painted in oils on a panel of walnut wood. ',
-    column: 0,
   },
   {
     name: 'The Boy in the Red Vest',
@@ -38,7 +35,6 @@ export const paints = [
 
     description:
       "The Boy in the Red Vest, also known as The Boy in the Red Waistcoat, is a painting by Paul Cézanne, painted in 1889 or 1890. It is a fine example of Cézanne's skilled, nuanced, and innovative mature work after 1880. ",
-    column: 0,
   },
 
   {
@@ -49,7 +45,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/1665_Girl_with_a_Pearl_Earring.jpg/540px-1665_Girl_with_a_Pearl_Earring.jpg',
     description:
       'Girl with a Pearl Earring is an oil painting by Dutch Golden Age painter Johannes Vermeer, dated c. 1665. Going by various names over the centuries, it became known by its present title towards the end of the 20th century after the earring worn by the girl portrayed there. ',
-    column: 1,
   },
   {
     name: 'The Great Wave off Kanagawa',
@@ -59,7 +54,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Tsunami_by_hokusai_19th_century.jpg/700px-Tsunami_by_hokusai_19th_century.jpg',
     description:
       'The Great Wave off Kanagawa is a woodblock print that was made by Japanese ukiyo-e artist Hokusai, probably in late 1831 during the Edo period of Japanese history. The print depicts three boats moving through a storm-tossed sea with a large wave forming a spiral in the centre and Mount Fuji visible in the background. ',
-    column: 1,
   },
   {
     name: 'The Night Cafe',
@@ -69,7 +63,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Vincent_Willem_van_Gogh_076.jpg/540px-Vincent_Willem_van_Gogh_076.jpg',
     description:
       'The Night Café is an oil painting created by Dutch artist Vincent van Gogh in September 1888 in Arles. Its title is inscribed lower right beneath the signature. The painting is owned by Yale University and is currently held at the Yale University Art Gallery in New Haven, Connecticut. ',
-    column: 1,
   },
   {
     name: 'Arnolfini Portrait',
@@ -79,7 +72,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/The_Arnolfini_portrait_%281434%29.jpg/540px-The_Arnolfini_portrait_%281434%29.jpg',
     description:
       'The Arnolfini Portrait is a 1434 oil painting on oak panel by the Early Netherlandish painter Jan van Eyck. It forms a full-length double portrait, believed to depict the Italian merchant Giovanni di Nicolao Arnolfini and his wife, presumably in their residence at the Flemish city of Bruges. ',
-    column: 1,
   },
 
   {
@@ -90,7 +82,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Mural_del_Gernika.jpg/660px-Mural_del_Gernika.jpg',
     description:
       'Guernica is a large 1937 oil painting by Spanish artist Pablo Picasso. It is one of his best-known works, regarded by many art critics as the most moving and powerful anti-war painting in history. Guernica is exhibited in the Museo Reina Sofía in Madrid.',
-    column: 2,
   },
   {
     name: 'Van Gogh Self-Portrait',
@@ -100,7 +91,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg/540px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg',
     description:
       "Dutch Post-Impressionist painter Vincent van Gogh painted a self-portrait in oil on canvas in September 1889. The work, which may have been Van Gogh's last self-portrait, was painted shortly before he left Saint-Rémy-de-Provence in southern France. The painting is now at the Musée d'Orsay in Paris.",
-    column: 2,
   },
   {
     name: 'Mona Lisa',
@@ -110,7 +100,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/540px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg',
     description:
       'The Mona Lisa is a half-length portrait painting by Italian artist Leonardo da Vinci. Considered an archetypal masterpiece of the Italian Renaissance, it has been described as "the best known, the most visited, the most written about, the most sung about, the most parodied work of art in the world".',
-    column: 2,
   },
 
   {
@@ -121,7 +110,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Artemisia_Gentileschi_%E2%80%94_Conversione_della_Maddalena_%28Maria_Maddalena_penitente.jpg/540px-Artemisia_Gentileschi_%E2%80%94_Conversione_della_Maddalena_%28Maria_Maddalena_penitente.jpg',
     description:
       'Penitent Magdalene is a painting by the Italian artist Artemisia Gentileschi. It hangs in Seville Cathedral. It has probably been in the cathedral since the late 17th century. She returned to the subject later in the 1620s in Mary Magdalene as Melancholy.',
-    column: 3,
   },
   {
     name: 'The Sleeping Gypsy',
@@ -131,7 +119,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/La_Boh%C3%A9mienne_endormie.jpg/540px-La_Boh%C3%A9mienne_endormie.jpg',
     description:
       'The Sleeping Gypsy is an 1897 oil painting by French Naïve artist Henri Rousseau. It is a fantastical depiction of a lion musing over a sleeping woman on a moonlit night. Rousseau first exhibited the painting at the 13th Salon des Indépendants, and tried unsuccessfully to sell it to the mayor of his hometown, Laval. ',
-    column: 3,
   },
   {
     name: 'The Basket of Apples',
@@ -141,7 +128,6 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Paul_C%C3%A9zanne_-_The_Basket_of_Apples_-_1926.252_-_Art_Institute_of_Chicago.jpg/540px-Paul_C%C3%A9zanne_-_The_Basket_of_Apples_-_1926.252_-_Art_Institute_of_Chicago.jpg',
     description:
       'The Basket of Apples is a still life oil painting by French artist Paul Cézanne, which he created c. 1893. The painting rejected realistic representation in favour of distorting objects to create multiple perspectives. This approach eventually influenced other art movements, including Fauvism and Cubism.',
-    column: 3,
   },
   {
     name: 'The Swing',
@@ -151,6 +137,5 @@ export const paints = [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg/600px-Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg',
     description:
       "The Swing, also known as The Happy Accidents of the Swing, is an 18th-century oil painting by Jean-Honoré Fragonard in the Wallace Collection in London. It is considered to be one of the masterpieces of the Rococo era, and is Fragonard's best-known work.",
-    column: 3,
   },
 ];
